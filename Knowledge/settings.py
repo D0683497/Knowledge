2b943654351432cd1e2678ff4ede72c90257d3d2
D0683497 <ff129043923@gmail.com>
2019-12-18 01:50:30 +0800

    關閉 debug

--- Knowledge/settings.py
+++ Knowledge/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l3u^kbrw!-7^9abbbnqybqaw)@o($w4i&7))0vrg2&)=ujh2n('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -126,6 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # AUTH
 
